67f587b2c77fbaf001d4cb38f4fc3c0a8b6ee838
Rafal Mroz <rafalmroz30@gmail.com>
2020-01-24 17:06:03 +0100

    extract time function from CurrentTimebox

--- src/components/CurrentTimebox.js
+++ src/components/CurrentTimebox.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Clock from "./Clock";
 import ProgressBar from './ProgressBar';
+import { getMinutesAndSecondsFromDurationInSeconds} from "../lib/time"
 
 class CurrentTimebox extends React.Component {
     constructor(props){
@@ -89,8 +90,8 @@ render() {
    const {title, totalTimeInMinutes, isEditable, onEdit} = this.props;
    const totalTimeInSeconds = totalTimeInMinutes * 60;
    const timeLeftInSeconds = totalTimeInSeconds - elapsedTimeInSeconds;
-   const minutesLeft = Math.floor(timeLeftInSeconds/60);
-   const secondsLeft = Math.floor(timeLeftInSeconds%60);
+   
+   const [minutesLeft, secondsLeft] = getMinutesAndSecondsFromDurationInSeconds(timeLeftInSeconds);
    const progressInPercent = (elapsedTimeInSeconds / totalTimeInSeconds) * 100.0;
          return (
       <div className={`CurrentTimebox ${isEditable ? "inactive" : ""}`}>
