200c3c76af63f2de74f3af1b59571e47de992482
Rafal Mroz <rafalmroz30@gmail.com>
2020-02-17 17:30:04 +0100

    fix: restore working aplication

--- src/components/Timebox.js
+++ src/components/Timebox.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 
-function Timebox({ title, totalTimeInMinutes, onDelete, onEdit }) {
+function Timebox({ title, totalTimeInMinutes, onDelete }) {
     if (totalTimeInMinutes < 0) {
         throw new Error("Czas musi być większy od zera!")
     }
