290f673a26dc32d7da530550ba92eb6e6ee4b497
Rafal Mroz <rafalmroz30@gmail.com>
2020-02-27 10:23:20 +0100

    feat: AxiosApi implementation

---
+++ src/api/AxiosTimeboxesAPI.js
@@ -0,0 +1,36 @@
+
+import axios from "axios";
+
+
+
+const BASE_URL = "http://localhost:4000/timeboxes"
+const AxiosTimeboxesAPI = {
+    getAllTimeboxes: async function () {
+        const response = await axios.get(BASE_URL);
+        const timeboxes = response.data;
+        return timeboxes;
+    },
+    addTimebox: async function (timeboxToAdd) {
+        const response = await axios.post(BASE_URL, timeboxToAdd);
+        const addedTimebox = response.data;
+        return addedTimebox;
+    },
+    replaceTimebox: async function (timeboxToReplace) {
+        if (!timeboxToReplace.id) {
+            throw new Error("Timebox has to have an id to be updated")
+        }
+        const response = await axios.put(`${BASE_URL}/${timeboxToReplace.id}`, timeboxToReplace);
+        const replacedTimebox = response.data;
+        return replacedTimebox;
+
+    },
+    removeTimebox: async function (timeboxToRemove) {
+        if (!timeboxToRemove.id) {
+            throw new Error("Timebox has to have an id to be updated")
+        }
+        await axios.delete(`${BASE_URL}/${timeboxToRemove.id}`);
+    }
+}
+
+export default AxiosTimeboxesAPI;
+
